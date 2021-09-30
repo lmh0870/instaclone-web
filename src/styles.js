6a38462d20000cfe315e9d0040e65ca54ab3f8e3
lmh0870 <lmh0870@naver.com>
2021-09-30 14:04:24 +0900

    Clean: Components

--- src/styles.js
+++ src/styles.js
@@ -2,8 +2,8 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
 export const lightTheme = {
-  fontColor: "#2c2c2c",
-  bgColor: "white",
+  accent: "#0095f6",
+  borderColor: "rgb(219, 219, 219)",
 };
 
 export const darkTheme = {
@@ -20,9 +20,11 @@ export const GlobalStyles = createGlobalStyle`
       box-sizing:border-box;
     }
     body {
-        background-color: #222831;
-        font-size:14px;
+      background-color: #FAFAFA;
+      font-size:14px;
        font-family: 'Roboto', 'Spoqa Han Sans', 'Spoqa Han Sans JP', 'Sans-serif'
+      color:rgb(38, 38, 38);
+
     }
     a {
       text-decoration: none;
